8f1bc5040fc7f483c634642e1c47a51da9588b21
karbz0ne <dev@blaszczyk.tk>
2017-03-21 22:57:01 +0100

    Fixed broken tests

--- src/containers/Question/QuestionContainer.test.js
+++ src/containers/Question/QuestionContainer.test.js
@@ -3,7 +3,7 @@ import expect from "expect";
 import {mount} from "enzyme";
 import QuestionContainer from "./QuestionContainer.js";
 
-describe('Game page tests:', () => {
+describe('Quiz page tests:', () => {
   const questions = {
     list: [
       {
@@ -47,9 +47,34 @@ describe('Game page tests:', () => {
     ],
     isFetching: false,
     error: false,
+    current: {
+      title: 'What sound does a dog make?',
+      id: 0,
+      score: 100,
+      answers: [
+        {
+          title: 'Meow',
+          id: 0
+        },
+        {
+          title: 'Woof',
+          id: 1
+        },
+        {
+          title: 'Hoo',
+          id: 2
+        }
+      ]
+  },
   };
 
-  const component = mount(<QuestionContainer questions={questions} />);
+  const goToNextQuestion = () => false;
+
+  const component = mount(
+    <QuestionContainer
+      questions={questions}
+      goToNextQuestion={goToNextQuestion}
+    />);
 
   it('Should render QuestionContainer component', () => {
     expect(component.length).toBeTruthy();
@@ -57,5 +82,25 @@ describe('Game page tests:', () => {
 
   it('Should have child', () => {
     expect(component.find('div.question-container').length).toEqual(1);
+  });
+
+  it('Should show loading element while fetching data', () => {
+    const componentWithLoader = mount(
+      <QuestionContainer
+        questions={Object.assign({}, questions, {isFetching: true})}
+        goToNextQuestion={goToNextQuestion} />
+    );
+
+    expect(componentWithLoader.find('p.loading').length).toEqual(1);
+  });
+
+  it('Should show information about no more questions', () => {
+    const componentWithEmptyCurrentQuestion = mount(
+      <QuestionContainer
+        questions={Object.assign({}, questions, {current: null})}
+        goToNextQuestion={goToNextQuestion} />
+    );
+
+    expect(componentWithEmptyCurrentQuestion.find('p').length).toEqual(1);
   });
 });
--- src/layout/QuizPage.test.js
+++ src/layout/QuizPage.test.js
@@ -5,46 +5,48 @@ import {QuizPage} from "./QuizPage.js";
 
 describe('Game page tests:', () => {
   const actions = {};
-  const questions = [
-    {
-      title: 'What sound does a dog make?',
-      id: 0,
-      score: 100,
-      answers: [
-        {
-          title: 'Meow',
-          id: 0
-        },
-        {
-          title: 'Woof',
-          id: 1
-        },
-        {
-          title: 'Hoo',
-          id: 2
-        }
-      ]
-    },
-    {
-      title: 'What sound does a cat make?',
-      id: 1,
-      score: 150,
-      answers: [
-        {
-          title: 'Meow',
-          id: 0
-        },
-        {
-          title: 'Woof',
-          id: 1
-        },
-        {
-          title: 'Hoo',
-          id: 2
-        }
-      ]
-    }
-  ];
+  const questions = {
+    list: [
+      {
+        title: 'What sound does a dog make?',
+        id: 0,
+        score: 100,
+        answers: [
+          {
+            title: 'Meow',
+            id: 0
+          },
+          {
+            title: 'Woof',
+            id: 1
+          },
+          {
+            title: 'Hoo',
+            id: 2
+          }
+        ]
+      },
+      {
+        title: 'What sound does a cat make?',
+        id: 1,
+        score: 150,
+        answers: [
+          {
+            title: 'Meow',
+            id: 0
+          },
+          {
+            title: 'Woof',
+            id: 1
+          },
+          {
+            title: 'Hoo',
+            id: 2
+          }
+        ]
+      }
+    ]
+  };
 
   const component = mount(<QuizPage questions={questions} actions={actions} />);
 
@@ -58,6 +60,16 @@ describe('Game page tests:', () => {
 
   it('Should show div wrapper', () => {
     expect(component.find('div').length).toEqual(1);
+  });
+
+  it('Should show href for fetching data', () => {
+    const componentWithoutQuestions = mount(
+      <QuizPage
+        questions={[]}
+        actions={actions} />
+    );
+
+    expect(componentWithoutQuestions.find('a').length).toEqual(1);
   });
 
   it('Should not show loading', () => {
